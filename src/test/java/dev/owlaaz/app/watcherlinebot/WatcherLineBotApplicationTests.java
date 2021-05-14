fd599a458257a1b81a87464cf45b7f89ad08ab88
Pongpon Khampan <pongpon.khampan@stream.co.th>
2021-05-14 16:04:49 +0700

    initial code based

---
+++ src/test/java/dev/owlaaz/app/watcherlinebot/WatcherLineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.owlaaz.app.watcherlinebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatcherLineBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
